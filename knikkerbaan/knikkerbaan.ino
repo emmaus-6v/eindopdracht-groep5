ae82f904f186ee613a3f4e3b42595a0be1223ace
MaartenVanKoesveld <111147@emmauscollege.nl>
2021-02-15 09:22:03 +0000

    for loop problemen, maar verder werkt het soort van

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,34 +1,81 @@
-const int ledPin =  LED_BUILTIN;
+/*
+  Sweep
 
-// globale variabelen
-int ledState = LOW;               // status van de LED
-unsigned long millisVorigeVerandering = 0; // bewaard tijd van laatste LED-verandering
+  by BARRAGAN <http://barraganstudio.com>
+  This example code is in the public domain.
 
-// andere constanten
-const long interval = 1000;           // interval in milliseconden
+  modified 8 Nov 2013  by Scott Fitzgerald
+  http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+String sorteerRichting = "stilstand"; //kan zijn stilstand, linksom en rechtsom 
+int pos1 = 90;
+int knop1 = 3;
+int knop2 = 2;
+
+Servo servo_8;
+
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+const unsigned long period = 1000;  //the value is a number of milliseconds
 
 void setup() {
-  // stel ledpin in als output:
-  pinMode(ledPin, OUTPUT);
+  servo_8.attach(8, 500, 2500);
+  servo_8.write(pos1);
+  pinMode(knop1, INPUT);
+  pinMode(knop2, INPUT);
+  startMillis = millis();  //initial start time
 }
 
 void loop() {
-  // tijd van nu
-  unsigned long millisNu = millis();
-
-  // is de verstreken tijd sinds de laatste verandering langer dan de interval?
-  if (millisNu - millisVorigeVerandering >= interval) {    
-    // bewaar tijdstip van de verandering die we hierna gaan doen
-    millisVorigeVerandering = millisNu;
-
-    // zet de status van de LED om
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // geef de pin de waarde van de status
-    digitalWrite(ledPin, ledState);
+  currentMillis = millis();
+  
+  if (digitalRead(knop1) == HIGH) {
+  	sorteerRichting = "linksom";
   }
+  
+  if (digitalRead(knop2) == HIGH) {
+    sorteerRichting = "rechtsom";
+  }
+  
+  
+  if (sorteerRichting == "linksom") {
+    servo_8.write(90);
+    
+    if (currentMillis - startMillis >= period) {
+      for (pos1 = 90; pos1 < 180; pos1 += 1) {
+        // tell servo to go to position in variable 'pos'
+        servo_8.write(pos1);
+        // wait 15 ms for servo to reach the position
+        delay(15); // Wait for 15 millisecond(s)
+      }
+      for (pos1 = 180; pos1 > 90; pos1 -=1) {
+        // tell servo to go to position in variable 'pos'
+        servo_8.write(pos1);
+        // wait 15 ms for servo to reach the position
+        delay(15); // Wait for 15 millisecond(s)
+      }
+    }
+  }
+  
+  if (sorteerRichting == "rechtsom") {
+    servo_8.write(90);
+
+    if (currentMillis - startMillis >= period) {
+      for (pos1 = 90; pos1 > 0; pos1 -= 1) {
+        // tell servo to go to position in variable 'pos'
+        servo_8.write(pos1);
+        // wait 15 ms for servo to reach the position
+        delay(15); // Wait for 15 millisecond(s)
+      }
+      for (pos1 = 0; pos1 < 90; pos1 +=1) {
+        // tell servo to go to position in variable 'pos'
+        servo_8.write(pos1);
+        // wait 15 ms for servo to reach the position
+        delay(15); // Wait for 15 millisecond(s)
+      }
+    }
+  } 
 }
